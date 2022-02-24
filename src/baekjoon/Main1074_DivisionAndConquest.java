dbf6f05d228f681005b047d3417f6e81083d1664
thwjd <thwjd1356@naver.com>
2022-02-24 15:24:43 +0900

    [백준] 1074 Z

--- src/baekjoon/Main1074_DivisionAndConquest.java
+++ src/baekjoon/Main1074_DivisionAndConquest.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main1074_DivisionAndConquest {
 
-    static int count  = -1;
+    static int count  = 0;
     static int findX;
     static int findY;
     static boolean find = false;
@@ -23,32 +23,43 @@ public class Main1074_DivisionAndConquest {
         //입력완료
 
         partition(0, 0, size);
-        System.out.println(count);
+        System.out.println(count-1);
     }
 
     private static void partition(int startX, int startY, int size) {
-        if(find == false){
-            if(size <= 2){
-                find = findXY(startX, startY);
-                return;
-            }
-        }else{
+        if(size <= 2){
+            findXY(startX, startY);
             return;
         }
 
-        //4등분
-        size  = size / 2;
-        partition(startX,                    startY,     size);    //좌측상단
-        partition(startX+size,        startY,      size);    //우측상단
-        partition(startX,             startY+size, size);    //좌측하단
-        partition(startX+size, startY+size, size);    //우측하단
+        int half = size/2;
+        boolean left =          startX <= findX && findX < startX + half;
+        boolean right =  startX + half <= findX && findX < startX + size;
+        boolean top =           startY <= findY && findY < startY + half;
+        boolean bottom = startY + half <= findY && findY < startY + size;
+
+
+        if(left && top){
+            partition(startX, startY, half);
+        }else if(right && top){
+            count = count + ((half * half) * 1);
+            partition(startX+half, startY, half);
+        }else if(left && bottom){
+            count = count + ((half * half) * 2);
+            partition(startX, startY+half, half);
+        }else if(right && bottom){
+            count = count + ((half * half) * 3);
+            partition(startX+half, startY+half, half);
+        }
+
     }
+
 
     private static boolean findXY(int startX, int startY){
 
         //Z 방향으로 확인
         for (int j = startY; j < startY + 2; j++) {     //Y가 먼저
-           for (int i = startX; i < startX+2; i++) {
+            for (int i = startX; i < startX+2; i++) {
                 count++;
                 if (i == findX && j == findY) {
                     return true;
